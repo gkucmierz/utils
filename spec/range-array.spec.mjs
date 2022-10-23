87b67f10008e2508fdfef262ef8e577fd81a691b
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-10-23 17:59:59 +0200

    add spec

---
+++ spec/range-array.spec.mjs
@@ -0,0 +1,12 @@
+
+import {
+  range2array,
+  array2range,
+} from '../src/range-array/range-array.mjs';
+
+describe('range-array', () => {
+  it('should work', function() {
+    expect(array2range([1,3,4,5,7,9,10])).toEqual([[1,1],[3,5],[7,7],[9,10]]);
+    expect(array2range([10,12,13,14,15,16,17,20,22,23,27])).toEqual([[10,10],[12,17],[20,20],[22,23],[27,27]]);
+  });
+});
