5307d227c9c080ecdec0324883cd8ff58eb532cf
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-11-01 22:07:11 +0100

    add powMod

---
+++ src/pow-mod.js
@@ -0,0 +1,14 @@
+
+export const powMod = (base, exponent, modulus) => {
+  if (modulus === 1) return 0;
+  let result = 1;
+  base = base % modulus;
+  while (exponent > 0) {
+    if (exponent % 2 === 1) {
+      result = (result * base) % modulus;
+    }
+    exponent = exponent >> 1;
+    base = (base * base) % modulus;
+  }
+  return result;
+};
