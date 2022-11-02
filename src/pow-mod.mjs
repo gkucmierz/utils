554c5164d858de18defc65bbd812d87d223e2d65
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-11-02 16:06:57 +0100

    add pow-mod big int

--- src/pow-mod.mjs
+++ src/pow-mod.mjs
@@ -1,4 +1,6 @@
 
+// implementation of power function with modulus like native python pow
+
 export const powMod = (base, exponent, modulus) => {
   if (modulus === 1) return 0;
   let result = 1;
@@ -8,6 +10,20 @@ export const powMod = (base, exponent, modulus) => {
       result = (result * base) % modulus;
     }
     exponent = exponent >> 1;
+    base = (base * base) % modulus;
+  }
+  return result;
+};
+
+export const powModBI = (base, exponent, modulus) => {
+  if (modulus === 1n) return 0n;
+  let result = 1n;
+  base = base % modulus;
+  while (exponent > 0n) {
+    if (exponent % 2n === 1n) {
+      result = (result * base) % modulus;
+    }
+    exponent = exponent / 2n;
     base = (base * base) % modulus;
   }
   return result;
