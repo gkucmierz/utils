9be0ad6c57c56fded06a479920aef761f1998b28
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-11-04 03:59:16 +0100

    different phi implementation

--- src/phi.mjs
+++ src/phi.mjs
@@ -4,6 +4,11 @@ import {
   gcdBI,
 } from './gcd.mjs';
 
+import {
+  factors,
+  factorsBI,
+} from './factors.mjs';
+
 // Euler's Totient Function
 
 const getPhi = (ONE, TWO, gcd) => {
@@ -18,5 +23,16 @@ const getPhi = (ONE, TWO, gcd) => {
   };
 }
 
-export const phi = getPhi(1, 2, gcd);
-export const phiBI = getPhi(1n, 2n, gcdBI);
+// Eulers formula
+
+const getPhiEuler = factors => {
+  return n => {
+    return [...new Set(factors(n))].reduce((res, f) => res - res / f, n);
+  };
+};
+
+export const phi = getPhiEuler(factors);
+export const phiBI = getPhiEuler(factorsBI);
+
+// export const phi = getPhi(1, 2, gcd);
+// export const phiBI = getPhi(1n, 2n, gcdBI);
