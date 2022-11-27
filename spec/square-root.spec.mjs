dfe017cc81d6b2d9ba910c75b2dcd9d712469d03
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-11-27 10:27:54 +0100

    fix: square-root

--- spec/square-root.spec.mjs
+++ spec/square-root.spec.mjs
@@ -5,11 +5,18 @@ import {
 } from '../src/square-root.mjs';
 
 describe('square-root', () => {
-  it('squareRootBI', () => {
+  it('squareRootBI small', () => {
     expect(squareRootBI(0n)).toEqual(0n);
     expect(squareRootBI(1n)).toEqual(1n);
     expect(squareRootBI(2n)).toEqual(1n);
     expect(squareRootBI(3n)).toEqual(1n);
     expect(squareRootBI(4n)).toEqual(2n);
+  });
+
+  it('squareRootBI 0-1e3', () => {
+    for (let i = 0; i < 1e3; ++i) {
+      const sqf = BigInt(Math.floor(i ** 0.5));
+      expect(squareRootBI(BigInt(i))).toEqual(sqf);
+    }
   });
 });
--- src/square-root.mjs
+++ src/square-root.mjs
@@ -6,12 +6,18 @@ export const squareRoot = n => n ** 0.5;
 export const squareRootBI = n => {
   if (n === 0n) return 0n;
   if (n < 4n) return 1n;
-  let res = n > 10n ? BigInt(n.toString().substr(0, n.toString().length / 2)) : 1n;
+  if (n < 9n) return 2n;
+  if (n < 16n) return 3n;
+  let res = BigInt(n.toString().substr(0, n.toString().length / 2));
   let last;
-  while (res * res - n !== 0n) {
+  while (true) {
     last = res;
     res = (res + n / res) / 2n;
-    if (res === last) return res;
+    const p = res * res;
+    if (p === n) return res;
+    if (last === res) return res;
+    const next = p + res * 2n - 1n;
+    if (n > next) return res;
   }
   return res;
 };
