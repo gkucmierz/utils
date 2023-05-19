46f9b9aedc81c5a4a5ef0810d5a1747ac9fe2445
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-05-19 14:13:22 +0200

    add binarySearchLE

--- spec/binary-search.spec.mjs
+++ spec/binary-search.spec.mjs
@@ -1,6 +1,7 @@
 
 import {
   binarySearchArr,
+  binarySearchLE,
 } from '../src/binary-search.mjs';
 
 describe('binarySearchArr', () => {
@@ -11,5 +12,33 @@ describe('binarySearchArr', () => {
     expect(binarySearchArr([1, 2, 3, 4], 4)).toBe(3);
     expect(binarySearchArr([1, 2, 3, 4], 0)).toBe(-1);
     expect(binarySearchArr([1, 2, 3, 4], 5)).toBe(-1);
+  });
+});
+
+describe('binarySearchLE', () => {
+  it('mid group', () => {
+    const arr = [0,1,2,2,3];
+    expect(binarySearchLE(arr, -100)).toBe(-1);
+    expect(binarySearchLE(arr, 0)).toBe(0);
+    expect(binarySearchLE(arr, 1)).toBe(1);
+    expect(binarySearchLE(arr, 2)).toBe(3);
+    expect(binarySearchLE(arr, 3)).toBe(4);
+    expect(binarySearchLE(arr, 100)).toBe(4);
+  });
+
+  it('begin group', () => {
+    const arr = [2,2,3];
+    expect(binarySearchLE(arr, -100)).toBe(-1);
+    expect(binarySearchLE(arr, 2)).toBe(1);
+    expect(binarySearchLE(arr, 3)).toBe(2);
+    expect(binarySearchLE(arr, 100)).toBe(2);
+  });
+
+  it('end group', () => {
+    const arr = [1,2,2];
+    expect(binarySearchLE(arr, -100)).toBe(-1);
+    expect(binarySearchLE(arr, 1)).toBe(0);
+    expect(binarySearchLE(arr, 2)).toBe(2);
+    expect(binarySearchLE(arr, 100)).toBe(2);
   });
 });
--- src/binary-search.mjs
+++ src/binary-search.mjs
@@ -1,4 +1,6 @@
 
+// binary search array exact element
+// in sorted array
 export const binarySearchArr = (arr, target) => {
   let [a, b] = [0, arr.length];
   let lm;
@@ -13,6 +15,24 @@ export const binarySearchArr = (arr, target) => {
       return mid;
     }
     if (lm === mid) break;
+    lm = mid;
+  }
+  return -1;
+};
+
+// binary search array less or equal element
+export const binarySearchLE = (arr, target) => {
+  let [a, b] = [0, arr.length];
+  let lm;
+  while (b - a > 0) {
+    const mid = (a + b) / 2 | 0;
+    const val = arr[mid];
+    if (target < val) {
+      b = mid;
+    } else if (val <= target) {
+      a = mid;
+    }
+    if (lm === mid) return mid;
     lm = mid;
   }
   return -1;
