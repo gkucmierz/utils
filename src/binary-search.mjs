4dbd8a841f22e758d616f64439f532f8c795abbe
Grzegorz Kućmierz <gkucmierz@gmail.com>
2024-06-17 14:07:08 +0200

    add binarySearchL, binarySearchG

--- src/binary-search.mjs
+++ src/binary-search.mjs
@@ -63,3 +63,39 @@ export const binarySearchRangeIncl = (arr, target) => {
     binarySearchLE(arr, target),
   ];
 };
+
+// binary search array less element
+const binarySearchL = (arr, target) => {
+  let [a, b] = [0, arr.length];
+  let lm;
+  while (b - a > 0) {
+    const mid = (a + b) / 2 | 0;
+    const val = arr[mid];
+    if (target <= val) {
+      b = mid;
+    } else if (val < target) {
+      a = mid;
+    }
+    if (lm === mid) return mid;
+    lm = mid;
+  }
+  return -1;
+};
+
+// binary search array greater element
+const binarySearchG = (arr, target) => {
+  let [a, b] = [0, arr.length];
+  let lm;
+  while (b - a > 0) {
+    const mid = (a + b) / 2 | 0;
+    const val = arr[mid];
+    if (target >= val) {
+      a = mid;
+    } else if (val > target) {
+      b = mid;
+    }
+    if (lm === mid) return mid + 1;
+    lm = mid;
+  }
+  return 0;
+};
