59a0136d07e1fde1c55634d4b200441f618e472d
Grzegorz Kucmierz <gkucmierz@gmail.com>
2025-11-18 15:05:00 +0100

    docs: mod

--- src/mod.mjs
+++ src/mod.mjs
@@ -1,6 +1,4 @@
 
-// python like mod implementation
-
 const getMod = ZERO => {
   return (dividend, divisor) => {
     if ((dividend < ZERO) ^ (divisor < ZERO)) {
@@ -11,5 +9,24 @@ const getMod = ZERO => {
   };
 };
 
+/**
+ * Python like modulo implementation.
+ * It behaves different than JavaScript %
+ * with negative values
+ * @method
+ * @param {Number} Dividend
+ * @param {Number} Divisor
+ * @return {Number} Modulus
+ */
 export const mod = getMod(0);
+
+/**
+ * Python like modulo implementation.
+ * It behaves different than JavaScript %
+ * with negative values
+ * @method
+ * @param {BigInt} Dividend
+ * @param {BigInt} Divisor
+ * @return {BigInt} Modulus
+ */
 export const modBI = getMod(0n);
